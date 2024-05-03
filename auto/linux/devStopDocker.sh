0d4bf0459dda34b6e4f61296084634b4b17cb6f9
Diego Ramirez <dieghoramreyes@gmail.com>
2024-05-02 19:53:25 -0600

    First commit

---
+++ auto/linux/devStopDocker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker stop dev_deployment_container
+docker rm dev_deployment_container
---
+++ auto/windows/devBuildDocker.bat
@@ -0,0 +1,8 @@
+@echo off
+
+docker stop dev_deployment_container
+docker rm dev_deployment_container
+
+docker build -t dev_deployment ../../
+
+docker run -d -p 8080:80 --name dev_deployment_container dev_deployment
